9e30e11f93cb1fb09bc321a020c95d35bdd03dba
Rick Umali <rickumali@gmail.com>
2024-07-18 19:32:02 -0400

    Makes label better
    rick@Mac:~/Projects/aws-tf $ ./listinstances.sh
    --------------------------------------------------------------------
    |                         DescribeInstances                        |
    +-----------+------------------------------------------------------+
    |  DNS      |  ec2-18-220-19-124.us-east-2.compute.amazonaws.com   |
    |  Instance |  i-065ad6e00a9228254                                 |
    |  Public IP|  18.220.19.124                                       |
    +-----------+------------------------------------------------------+
    
    Don't ask me about the formatting!

--- listinstances.sh
+++ listinstances.sh
@@ -1,4 +1,4 @@
 aws ec2 describe-instances \
     --filter Name=instance-state-name,Values=running \
-    --query 'Reservations[*].Instances[*].{Instance:InstanceId,IPAddress:PublicIpAddress,DNS:PublicDnsName}' \
+    --query 'Reservations[*].Instances[*].{Instance:InstanceId,"Public IP":PublicIpAddress,DNS:PublicDnsName}' \
     --output table
