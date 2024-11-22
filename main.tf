cceac3f58f739042f7e260a2d18ac008656ad0bb
Rick Umali <rickumali@gmail.com>
2024-11-21 21:15:21 -0500

    Updates egress to reference ELB Security Group
    See adc6aed1

--- main.tf
+++ main.tf
@@ -23,12 +23,12 @@ resource "aws_security_group" "ec2_secgrp" {
     security_groups = [aws_security_group.elb_secgrp.id]
   }
 
-  // This enables full egress
+  // Outbound HTTP goes to ELB only
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.elb_secgrp.id]
   }
 }
 
