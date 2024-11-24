f51d6aca7307ec4c5d7919deb8a96a73079b01f0
Rick Umali <rickumali@gmail.com>
2024-11-23 19:33:02 -0500

    Outputs new domain name

--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "vpc_id" {
   value       = data.aws_vpc.default.id
   description = "The Default VPC's ID"
 }
+
+output "domain_name" {
+  value       = aws_acm_certificate.devcert.domain_name
+  description = "The newly created domain name"
+}
