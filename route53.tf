e6e4d8a8fd667610219befe01d1e782e3cca9788
Rick Umali <rickumali@gmail.com>
2024-11-30 22:16:45 -0500

    Provisions httpbin Route 53 domain
    This is hardcoded for testing. I'll parameterize this in a variable
    later!

--- route53.tf
+++ route53.tf
@@ -13,3 +13,15 @@ resource "aws_route53_record" "example_route53_record" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "httpbin_route53_record" {
+  zone_id = data.aws_route53_zone.existing_zone.id
+  name    = "httpbin.test.gitmol.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.example_alb.dns_name
+    zone_id                = aws_lb.example_alb.zone_id
+    evaluate_target_health = true
+  }
+}
